04fbeddb115206468b535d1691baa4443b0f518b
mintlattice <SinatraMcmurdo84@hotmail.com>
2025-10-18 00:19:47 +0800

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedRanking = await deploy("EncryptedRanking", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedRanking contract: `, deployedRanking.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encrypted_ranking"; // id required to prevent reexecution
+func.tags = ["EncryptedRanking"];
