155fdca88befb62c22c5e16469de5eef78379e17
silverquills <VacheEgil02071995ma@hotmail.com>
2025-10-18 00:22:30 +0800

    join ui

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import '../styles/Header.css';
+
+export function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-content">
+          <div className="header-left">
+            <h1 className="header-title">Encrypted Ranking Portal</h1>
+          </div>
+          <ConnectButton />
+        </div>
+      </div>
+    </header>
+  );
+}
